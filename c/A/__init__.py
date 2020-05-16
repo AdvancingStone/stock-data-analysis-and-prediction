b2c0480f3ab3de3562eea09a173fb9e8b12e2d59
liushuai <banaana@qq.com>
2020-05-17 04:28:52 +0800

    remove tmp file

--- c/A/__init__.py
+++
@@ -1,5 +0,0 @@
-
-import os,sys
-Path=os.path.dirname(os.path.abspath(__file__))
-sys.path.append(Path)
-from A1 import *
--- c/B/B1.py
+++
@@ -1,8 +0,0 @@
-
-import os,sys
-Path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-sys.path.append(Path)
-from A import *
-
-a()
