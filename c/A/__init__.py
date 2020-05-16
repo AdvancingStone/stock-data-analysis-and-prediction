83ffa66e0eda638ed026edd0e38640406beb7d02
liushuai <banaana@qq.com>
2020-05-17 01:35:27 +0800

    aa

---
+++ c/A/__init__.py
@@ -0,0 +1,5 @@
+
+import os,sys
+Path=os.path.dirname(os.path.abspath(__file__))
+sys.path.append(Path)
+from A1 import *
---
+++ c/B/B1.py
@@ -0,0 +1,8 @@
+
+import os,sys
+Path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append(Path)
+from A import *
+
+a()
