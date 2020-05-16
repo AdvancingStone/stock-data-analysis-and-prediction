811693fe37fdcaa95902052ead14fd8d3137eb35
liushuai <banaana@qq.com>
2020-05-17 02:14:34 +0800

    fix bugfix

--- src/main/python/com/bluehonour/utils/__init__.py
+++ src/main/python/com/bluehonour/utils/__init__.py
@@ -1,6 +1,7 @@
 import os, sys
-Path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(Path)
-from com.bluehonour.utils.readConfig import *
-from com.bluehonour.utils.date_to_weekday import *
-from com.bluehonour.utils.get_stock_data_path import *
+utilspath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(utilspath)
+
+from readConfig import *
+from date_to_weekday import *
+from get_stock_data_path import *
