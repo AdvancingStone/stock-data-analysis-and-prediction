5f850428516bcba51002a1ab7cc94b00cb42ba20
liushuai <banaana@qq.com>
2020-07-05 23:43:05 +0800

    add bx_top10 of rise and volume for summary

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -201,14 +201,6 @@ fields terminated by '\t'
 stored as textfile;
 
 
-	code as `股票代码`,
-	name as `股票名称`,
-	count(*) as `上榜次数`,
-	max(a.zdf) as `最大涨跌幅`,
-	min(a.zdf) as `最小涨跌幅`,
-	collect_set(dt) as `日期集`
-
-
 create table if not exists stock.top10(
 code string comment '',
 name string comment '',
